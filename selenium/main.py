6a6263654721d4bdfc5ec6109c277c5d9b19d4a0
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-03-24 22:44:32 +0000

    - added timelapse

--- selenium/main.py
+++ selenium/main.py
@@ -24,21 +24,30 @@ def login():
     sign_element = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
     sign_element.click()
 
+    timesleep(2)
     quiz()
 
 def quiz():
     element = driver.find_element(By.CSS_SELECTOR, 'a[class="nav-link collapsed"]')
     element.click()
 
+    timesleep(2)
+
     element = driver.find_element(By.CSS_SELECTOR, 'a[href="/questionario_tecnicomanutencao"]')
     element.click()
+
+    timesleep(2)
 
     number_member_element = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Número Técnico"]')
     number_member_element.send_keys('28244')
 
+    timesleep(1)
+
     select_element = driver.find_element(By.CSS_SELECTOR, 'select[id="pl"]')
     select = Select(select_element)
     select.select_by_value('o')
+
+    timesleep(2)
 
     check_element_1 = driver.find_element(By.CSS_SELECTOR, 'input[name="check[]"]')
     check_element_1.click()
@@ -46,17 +55,23 @@ def quiz():
     check_element_2 = driver.find_element(By.CSS_SELECTOR, 'input[name="check2[]"]')
     check_element_2.click()
 
+    timesleep(2)
+    
     check_element_3 = driver.find_element(By.CSS_SELECTOR, 'input[name="check4[]"]')
     check_element_3.click()
 
     check_element_4 = driver.find_element(By.CSS_SELECTOR, 'input[name="check6[]"]')
     check_element_4.click()
 
+    timesleep(2)
+
     check_element_5 = driver.find_element(By.CSS_SELECTOR, 'input[name="check8[]"]')
     check_element_5.click()
 
     check_element_6 = driver.find_element(By.CSS_SELECTOR, 'input[name="check10[]"]')
     check_element_6.click()
+
+    timesleep(2)
 
     check_element_7 = driver.find_element(By.CSS_SELECTOR, 'input[name="check12[]"]')
     check_element_7.click()
@@ -67,10 +82,18 @@ def quiz():
     check_element_9 = driver.find_element(By.CSS_SELECTOR, 'input[name="check19[]"]')
     check_element_9.click()
 
+    timesleep(2)
+
     submit_element = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
     submit_element.click()
 
-login()
+    closebot()
 
-time.sleep(20)
-driver.quit()
+def timesleep(args):
+    time.sleep(args)
+
+def closebot():
+    time.sleep(10)
+    driver.quit()
+
+login()
