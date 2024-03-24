03259d1ece60dda6b67494f9f7b6f7452b14b1f3
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-03-24 11:17:49 +0000

    - added interaction with app quiz - click to interact - fill the quiz - submit the quiz
    - error found (name error: flash not defined)

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,8 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+import random
 import time
 
 service = Service(executable_path="chromedriver.exe")
@@ -9,17 +11,66 @@ driver = webdriver.Chrome(service=service)
 driver.get("http://127.0.0.1:5000/")
 driver.maximize_window()
 
-element = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-info btn-lg"]')
-element.click()
+def login():
+    element = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-info btn-lg"]')
+    element.click()
 
-username_element = driver.find_element(By.CSS_SELECTOR, 'input[id="username"]')
-username_element.send_keys('root')
+    username_element = driver.find_element(By.CSS_SELECTOR, 'input[id="username"]')
+    username_element.send_keys('root')
 
-password_element = driver.find_element(By.CSS_SELECTOR, 'input[id="password"]')
-password_element.send_keys('root')
+    password_element = driver.find_element(By.CSS_SELECTOR, 'input[id="password"]')
+    password_element.send_keys('root')
 
-sign_element = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-sign_element.click()
+    sign_element = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+    sign_element.click()
 
-time.sleep(10)
+    quiz()
+
+def quiz():
+    element = driver.find_element(By.CSS_SELECTOR, 'a[class="nav-link collapsed"]')
+    element.click()
+
+    element = driver.find_element(By.CSS_SELECTOR, 'a[href="/questionario_tecnicomanutencao"]')
+    element.click()
+
+    number_member_element = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Número Técnico"]')
+    number_member_element.send_keys('28244')
+
+    select_element = driver.find_element(By.CSS_SELECTOR, 'select[id="pl"]')
+    select = Select(select_element)
+    select.select_by_value('o')
+
+    check_element_1 = driver.find_element(By.CSS_SELECTOR, 'input[name="check[]"]')
+    check_element_1.click()
+
+    check_element_2 = driver.find_element(By.CSS_SELECTOR, 'input[name="check2[]"]')
+    check_element_2.click()
+
+    check_element_3 = driver.find_element(By.CSS_SELECTOR, 'input[name="check4[]"]')
+    check_element_3.click()
+
+    check_element_4 = driver.find_element(By.CSS_SELECTOR, 'input[name="check6[]"]')
+    check_element_4.click()
+
+    check_element_5 = driver.find_element(By.CSS_SELECTOR, 'input[name="check8[]"]')
+    check_element_5.click()
+
+    check_element_6 = driver.find_element(By.CSS_SELECTOR, 'input[name="check10[]"]')
+    check_element_6.click()
+
+    check_element_7 = driver.find_element(By.CSS_SELECTOR, 'input[name="check12[]"]')
+    check_element_7.click()
+
+    check_element_8 = driver.find_element(By.CSS_SELECTOR, 'input[name="check14[]"]')
+    check_element_8.click()
+
+    check_element_9 = driver.find_element(By.CSS_SELECTOR, 'input[name="check19[]"]')
+    check_element_9.click()
+
+    submit_element = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+    submit_element.click()
+
+login()
+
+time.sleep(20)
 driver.quit()
